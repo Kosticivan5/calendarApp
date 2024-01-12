d69347a525c45a45756d427cdda71c3c11ecdf89
kosticivan5 <kosticivan5@gmail.com>
2024-01-13 00:42:56 +0300

    made function for multiday event

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -1,9 +1,57 @@
 import dayjs from "dayjs";
+import { useEffect, useState } from "react";
 
 const Event = ({ name, start_date, finish_date }) => {
+  const [multiWeek, setMultiWeek] = useState(false);
+
   const eventStart = dayjs(start_date).day();
-  const eventEnd = dayjs(finish_date).day() - dayjs(start_date).day();
+  // const eventEnd = dayjs(finish_date).day() - dayjs(start_date).day();
+  const eventEnd = dayjs(finish_date).diff(dayjs(start_date), "day");
   const titleLimit = name.substring(0, 18);
+
+  // console.log(dayjs(start_date).day(), dayjs(finish_date).day());
+  // console.log(findate);
+  const eventEndDate = parseInt(dayjs(finish_date).format("D"), 10);
+  const eventStartDate = parseInt(dayjs(start_date).format("D"), 10);
+
+  // console.log(
+  //   dayjs(start_date).format("YY-MM-DD"),
+  //   dayjs(start_date).endOf("month").format("YY-MM-DD")
+  // );
+
+  const isMultiWeekEvent = (start, span) => {
+    const allowedToSpan = 6 - start;
+
+    if (span > allowedToSpan) {
+      setMultiWeek(true);
+    }
+  };
+
+  useEffect(() => {
+    const isMultiWeekEvent = (start, span) => {
+      const allowedToSpan = 6 - start;
+
+      if (span > allowedToSpan) {
+        setMultiWeek(true);
+      }
+    };
+    isMultiWeekEvent(eventStart, eventEnd);
+  }, [multiWeek]);
+
+  console.log(multiWeek);
+
+  console.table(
+    dayjs(start_date).format("YY-MM-DD"),
+    "___",
+    dayjs(start_date).day(),
+    "___",
+    dayjs(finish_date).format("YY-MM-DD"),
+    "___",
+    dayjs(finish_date).diff(dayjs(start_date), "day")
+  );
+
+  // console.log(dayjs("2024-01-31").week());
+  // console.log(dayjs("2024-01-31").add(1, "week").format("YY-MM-DD"));
 
   return (
     <button
@@ -15,7 +63,11 @@ const Event = ({ name, start_date, finish_date }) => {
       data-start={eventStart}
       data-span={eventEnd}
     >
-      <span>{eventEnd > 1 ? name : `${titleLimit}...`}</span>
+      <span>
+        {eventEnd > 1 || name.length <= titleLimit.length
+          ? name
+          : `${titleLimit}...`}
+      </span>
     </button>
   );
 };
